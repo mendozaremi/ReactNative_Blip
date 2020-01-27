f1c062a9ee4d37ec766e2380c967e8de8c9fb57d
Remi Mendoza <mendozaremi@gmail.com>
2020-01-27 10:18:39 -0500

    navigation works

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import React, { Component, useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { AppLoading } from 'expo';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import MapView from 'react-native-maps'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function HelloWorldApp() {
 
+class HomeScreen extends Component {
+  render() {
     return (
       <View style={styles.container}>
         <Text style={styles.blipStyle}>B L I P.</Text>
-        <TouchableOpacity
+        <Button
+          title="E N T E R"
           style={styles.button}
-          onPress={styles.button}
-          ><Text> E N T E R </Text>
-        </TouchableOpacity>
+          onPress={()=> {
+            console.log('pressed')
+            this.props.navigation.navigate('Map')
+          }}
+          >
+        </Button>
       </View>
     );
+  }
 }
 
+// =====================MAP=============================
+const Map = () => {
+  return (
+    <MapView style={{flex: 1}}
+      region={{latitude: 42.882004,
+      longitude: 74.582748,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+      }}
+      showsUserLocation={true}
+      />
+    );
+  }
+
+  // ==================ROOT STACK=========================
+
+  const RootStack = createStackNavigator(
+    {
+      Home: HomeScreen,
+      Map: Map,
+    },
+    {
+      initialRouteName: 'Home',
+    }
+  );
+
+  // ===================APP CONTAINER======================
+
+  const AppContainer = createAppContainer(RootStack)
+
+  export default class App extends React.Component {
+    render() {
+      return <AppContainer />
+    }
+  }
+
+// ++++=================STYLING===========================
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#2D5471',
